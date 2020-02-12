c2683ea471c5801301c03b0c66c5ab887bf46561
Cibi Aananth <cibiaananth@gmail.com>
2020-02-12 17:04:52 +0530

    feat: added minimap window scroll behaviour

--- src/pages/Photos/Konva.js
+++ src/pages/Photos/Konva.js
@@ -1,23 +1,80 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable react/button-has-type */
-import React, { createRef, useReducer } from 'react';
+import React, {
+  createRef,
+  useReducer,
+  useEffect,
+  useCallback,
+  useRef
+} from 'react';
 import PropTypes from 'prop-types';
 import { sumBy } from 'lodash';
+// components
 import MiniMap from './MiniMap';
 import ShelfCanvas from './ShelfCanvas';
 
+const scaleDownValues = (value, factor) => {
+  return value / factor;
+};
+
+const scaleUpValues = (value, factor) => {
+  return value * factor;
+};
+
 const initialState = {
-  type: null
+  miniMap: {
+    isWindowDragActive: false,
+    activeArea: [0, 0],
+    activeMouseEvent: ''
+  },
+  shelfCanvas: {
+    width: 0,
+    activeArea: [0, 0]
+  }
 };
 
 const reducer = (state, action) => {
   switch (action.type) {
-    case 'move':
-      return { type: 'move' };
-    case 'up':
-      return { type: 'up' };
-    case 'down':
-      return { type: 'down' };
+    case 'REGISTER_SHELF_CANVAS':
+      return {
+        ...state,
+        shelfCanvas: { ...state.shelfCanvas, ...action.payload },
+        miniMap: {
+          ...state.miniMap,
+          activeArea: [
+            scaleDownValues(
+              action.payload.activeArea[0],
+              action.payload.scaleDownFactor
+            ),
+            scaleDownValues(
+              action.payload.activeArea[1],
+              action.payload.scaleDownFactor
+            )
+          ]
+        }
+      };
+    case 'MINIMAP_WINDOW_DRAG': {
+      return {
+        ...state,
+        shelfCanvas: {
+          ...state.shelfCanvas,
+          activeArea: [
+            scaleUpValues(
+              action.payload.activeArea[0],
+              action.payload.scaleDownFactor
+            ),
+            scaleUpValues(
+              action.payload.activeArea[1],
+              action.payload.scaleDownFactor
+            )
+          ]
+        },
+        miniMap: {
+          ...state.miniMap,
+          activeArea: [...action.payload.activeArea]
+        }
+      };
+    }
     default:
       return state;
   }
@@ -25,18 +82,52 @@ const reducer = (state, action) => {
 
 const Konva = ({ list }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
+  const dispatchLogger = params => {
+    dispatch(params);
+  };
 
-  console.log('state', state);
-  const shelfCanvasRef = createRef();
-  const miniMapCanvasRef = createRef();
-
+  // function scope
   const miniMapScaleDownFactor = 10;
 
+  const shelfCanvasRef = createRef();
   const shelfCanvasWidth = sumBy(list.images, x => x.width);
   const shelfCanvasHeight = list.images[0].height;
 
-  const miniMapCanvasWidth = shelfCanvasWidth / miniMapScaleDownFactor;
-  const miniMapCanvasHeight = shelfCanvasHeight / miniMapScaleDownFactor;
+  const miniMapCanvasRef = createRef();
+  const miniMapCanvasWidth = scaleDownValues(
+    shelfCanvasWidth,
+    miniMapScaleDownFactor
+  );
+  const miniMapCanvasHeight = scaleDownValues(
+    shelfCanvasHeight,
+    miniMapScaleDownFactor
+  );
+
+  let updateState = useCallback(() => {
+    const { current } = shelfCanvasRef;
+    const { width } = current.getBoundingClientRect();
+    dispatchLogger({
+      type: 'REGISTER_SHELF_CANVAS',
+      payload: {
+        width,
+        activeArea: [current.scrollLeft, current.scrollLeft + width],
+        scaleDownFactor: miniMapScaleDownFactor
+      }
+    });
+  }, [shelfCanvasRef]);
+  updateState = useRef(updateState).current;
+
+  useEffect(() => {
+    updateState();
+  }, [updateState]);
+
+  useEffect(() => {
+    shelfCanvasRef.current.scrollTo({
+      top: 0,
+      left: state.shelfCanvas.activeArea[0],
+      behavior: 'smooth'
+    });
+  }, [state.shelfCanvas]);
 
   const handleButtonClick = ref => {
     ref.current.scrollBy({
@@ -54,14 +145,16 @@ const Konva = ({ list }) => {
         dispatch={dispatch}
         handleButtonClick={handleButtonClick}
         list={list}
+        reducerState={state}
         wrapperRef={shelfCanvasRef}
       />
       <div style={{ height: 40 }} />
       <MiniMap
         canvasHeight={miniMapCanvasHeight}
         canvasWidth={miniMapCanvasWidth}
-        dispatch={dispatch}
+        dispatch={dispatchLogger}
         list={list}
+        reducerState={state}
         scaleDownFactor={miniMapScaleDownFactor}
         wrapperRef={miniMapCanvasRef}
       />
@@ -82,3 +175,21 @@ Konva.propTypes = {
 };
 
 export default Konva;
+
+// let moveShelfCanvas = useCallback(() => {
+//   const { current } = shelfCanvasRef;
+//   current.scrollTo({
+//     top: 0,
+//     left: state.shelfCanvas.activeArea[0],
+//     behavior: 'smooth'
+//   });
+// }, [shelfCanvasRef, state.shelfCanvas.activeArea]);
+// moveShelfCanvas = useRef(moveShelfCanvas).current;
+
+// useEffect(() => {
+//   updateState();
+// }, [updateState]);
+
+// useEffect(() => {
+//   moveShelfCanvas();
+// }, [moveShelfCanvas]);
--- src/pages/Photos/MiniMap.js
+++ src/pages/Photos/MiniMap.js
@@ -2,7 +2,7 @@
 import React, { useRef } from 'react';
 import PropTypes from 'prop-types';
 import { throttle } from 'lodash';
-import { Stage, Layer } from 'react-konva';
+import { Layer, Rect, Stage } from 'react-konva';
 // pages
 import StitchedImage from './StitchedImage';
 
@@ -11,27 +11,50 @@ const MiniMap = ({
   canvasWidth,
   dispatch,
   list,
+  reducerState,
   scaleDownFactor,
   wrapperRef
 }) => {
-  const throttledEvents = useRef(throttle(q => handleMouseEvents(q), 300))
-    .current;
+  const throttledEvents = useRef(throttle(q => handleRectDrag(q), 300)).current;
 
-  const handleMouseEvents = ({ evt, type }) => {
-    dispatch({ payload: { evt }, type });
+  const [rectStart, rectEnd] = reducerState.miniMap.activeArea;
+
+  const handleRectDrag = ({
+    position: { x },
+    rectEnd: end,
+    rectStart: start
+  }) => {
+    dispatch({
+      payload: { activeArea: [x, end + x - start], scaleDownFactor },
+      type: 'MINIMAP_WINDOW_DRAG'
+    });
   };
 
   return (
     <div ref={wrapperRef}>
       <Stage width={canvasWidth} height={canvasHeight}>
-        <Layer
-          onMouseMove={({ evt }) => throttledEvents({ evt, type: 'move' })}
-          onMouseDown={({ evt }) => throttledEvents({ evt, type: 'down' })}
-          onMouseUp={({ evt }) => throttledEvents({ evt, type: 'up' })}
-        >
+        <Layer>
           <StitchedImage
             photoList={list.images}
             scaleDownFactor={scaleDownFactor}
+          />
+          <Rect
+            x={0}
+            y={0}
+            width={rectEnd - rectStart}
+            height={canvasHeight}
+            stroke="red"
+            draggable
+            dragBoundFunc={({ x }) => {
+              const minPos = 0;
+              const maxPos = canvasWidth - (rectEnd - rectStart);
+              const position = {
+                x: x <= minPos ? 0 : x >= maxPos ? maxPos : x,
+                y: 0
+              };
+              throttledEvents({ position, rectEnd, rectStart });
+              return position;
+            }}
           />
         </Layer>
       </Stage>
@@ -44,6 +67,7 @@ MiniMap.propTypes = {
   canvasHeight: PropTypes.number.isRequired,
   dispatch: PropTypes.func.isRequired,
   list: PropTypes.object.isRequired,
+  reducerState: PropTypes.object.isRequired,
   scaleDownFactor: PropTypes.number.isRequired,
   wrapperRef: PropTypes.any.isRequired
 };
